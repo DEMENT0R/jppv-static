642774098b755e00aec9bf0fe3c574f287b98b08
DEMENTOR <dmntr@rambler.ru>
2019-01-30 23:41:06 +1000

    mobile menu fix

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,12 @@
     <header>
       <nav class="navbar navbar-expand-lg row">
 
-        <div class="col-1 offset-1 row">
+        <div class="col-12 col-md-1 offset-md-1 row">
           <button class="navbar-toggler row" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <img src="img/icons/menu.svg" class="ml-2">
             <!-- <span class="navbar-toggler-icon">...</span> -->
           </button>
-          <a class="navbar-brand" href="/"><img src="img/icons/logo.svg" alt="JPPV" class="top-logo-img"></a>
+          <a class="navbar-brand" href="/"><img src="img/icons/logo.svg" alt="JPPV" class="top-logo-img" align="center"></a>
         </div>
         <div class="d-none d-md-block col-2 jppv-subheader">Платформа для поддержки<br>японских инвестиций</div>
         <div class="d-none d-md-block col-1">Jp</div>

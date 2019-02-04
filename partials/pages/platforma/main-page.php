e54de7bd45834d67ef335f89f8ae2b6dd647ee48
DEMENT0R <dmntr@rambler.ru>
2019-02-04 10:23:01 +1000

    map block hide and show

--- partials/pages/platforma/main-page.php
+++ partials/pages/platforma/main-page.php
@@ -60,46 +60,91 @@
       <div id="land_3" class="landingItem bg_3">
     			<!-- <div class="decoy">Карта</div> -->
           <div class="block-230"></div>
-          <div>
-          <div class="main-page-map-image-0 row">
-            <div class="col-1 bg-white"></div>
-            <div class="col-3 offset-1">
-              <h2 class="text-white mt-5">Дальний Восток</h2>
-              <div class="block-100"></div>
-              <h3 class="text-white">Социально экономическая информация</h3>
-              <p class="text-white">
-                Дальний Восток – самая восточная часть территории России. Представляет собой крайний северо-восток Азии. Макрорегион имеет выход к двум океанам: Тихому и Северному Ледовитому, граничит с четырьмя государствами (Китаем, Японией, США, КНДР).
+          <div class=" d-none d-md-block">
+            <div id="map-dv" class="main-page-map-image-0 row">
+              <div class="col-1 bg-white"></div>
+              <div class="col-3 offset-1">
+                <h2 class="text-white mt-5">Дальний Восток</h2>
+                <div class="block-100"></div>
+                <h3 class="text-white">Социально экономическая информация</h3>
+                <p class="text-white">
+                  Дальний Восток – самая восточная часть территории России. Представляет собой крайний северо-восток Азии. Макрорегион имеет выход к двум океанам: Тихому и Северному Ледовитому, граничит с четырьмя государствами (Китаем, Японией, США, КНДР).
 
-                9 дальневосточных регионов расположены в четырех часовых поясах и самых разных климатических зонах от арктических пустынь до субтропиков.
-              </p>
-              <a href="#" class="btn btn-info">Будет кнопка</a>
+                  9 дальневосточных регионов расположены в четырех часовых поясах и самых разных климатических зонах от арктических пустынь до субтропиков.
+                </p>
+                <a href="#" class="btn btn-info">Будет кнопка</a>
+              </div>
+              <div class="col-5">
+                <!-- Image Map -->
+                <img src="img/dv_map.svg" usemap="#image-map">
+
+                <map name="image-map">
+                    <area id="map-dv-btn" target="" alt="VL" title="VL" href="#" coords="327,729,336,720,325,691,332,679,348,677,352,641,358,629,371,634,389,616,381,606,396,595,402,613,401,644,386,693,363,719,347,714,334,735" shape="poly">
+                </map>
+                <script>
+                  $("#map-dv-btn").click(function(){
+                    $("#map-dv").hide();
+                    $("#map-vl").show();
+                  });
+                </script>
+              </div>
+              <div class="col-2">
+                <div class="block-100"></div>
+                <h2 class="text-white">6100 тыс. кв. км</h2>
+                <hr class="bg-white">
+                <h3 class="text-white">ПЛОЩАДЬ ДАЛЬНЕГО ВОСТОКА</h3>
+                <p class="text-white">Высокий ВРП - 268 000 руб. (на 19% выше среднего показателя по России)</p>
+
+                <h2 class="text-white mt-4">6,6  чел/кв. км</h2>
+                <hr class="bg-white">
+                <h3 class="text-white">НАСЕЛЕНИЕ РЕГИОНА</h3>
+                <p class="text-white">Первое место в России по объему инвестиций на душу населения</p>
+
+                <h2 class="text-white mt-4">>1 млн. чел</h2>
+                <hr class="bg-white">
+                <h3 class="text-white">ПЛОТНОСТЬ НАСЕЛЕНИЯ</h3>
+                <p class="text-white">Уровень зарплаты выше среднего показателя по России на 23%</p>
+              </div>
             </div>
-            <div class="col-5">
-              <!-- Image Map -->
-              <img src="img/dv_map.png" usemap="#image-map">
+            <div id="map-vl" class="main-page-map-image-1 row d-none">
+              <div class="col-1 bg-white"></div>
+              <div class="col-3 offset-1">
+                <h2 class="text-white mt-5">Дальний Восток</h2>
+                <div class="block-100"></div>
+                <h3 class="text-white">Социально экономическая информация</h3>
+                <p class="text-white">
+                  Дальний Восток – самая восточная часть территории России. Представляет собой крайний северо-восток Азии. Макрорегион имеет выход к двум океанам: Тихому и Северному Ледовитому, граничит с четырьмя государствами (Китаем, Японией, США, КНДР).
 
-              <map name="image-map">
-                  <area target="" alt="VL" title="VL" href="#" coords="327,729,336,720,325,691,332,679,348,677,352,641,358,629,371,634,389,616,381,606,396,595,402,613,401,644,386,693,363,719,347,714,334,735" shape="poly">
-              </map>
+                  9 дальневосточных регионов расположены в четырех часовых поясах и самых разных климатических зонах от арктических пустынь до субтропиков.
+                </p>
+                <a href="#" class="btn btn-info">Будет кнопка</a>
+              </div>
+              <div class="col-5">
+                <!-- Image Map -->
+                <img src="img/dv_map_vl.svg" usemap="#image-map">
+
+                <map name="image-map">
+                    <area target="" alt="VL" title="VL" href="#" coords="327,729,336,720,325,691,332,679,348,677,352,641,358,629,371,634,389,616,381,606,396,595,402,613,401,644,386,693,363,719,347,714,334,735" shape="poly">
+                </map>
+              </div>
+              <div class="col-2">
+                <div class="block-100"></div>
+                <h2 class="text-white">6100 тыс. кв. км</h2>
+                <hr class="bg-white">
+                <h3 class="text-white">ПЛОЩАДЬ ДАЛЬНЕГО ВОСТОКА</h3>
+                <p class="text-white">Высокий ВРП - 268 000 руб. (на 19% выше среднего показателя по России)</p>
+
+                <h2 class="text-white mt-4">6,6  чел/кв. км</h2>
+                <hr class="bg-white">
+                <h3 class="text-white">НАСЕЛЕНИЕ РЕГИОНА</h3>
+                <p class="text-white">Первое место в России по объему инвестиций на душу населения</p>
+
+                <h2 class="text-white mt-4">>1 млн. чел</h2>
+                <hr class="bg-white">
+                <h3 class="text-white">ПЛОТНОСТЬ НАСЕЛЕНИЯ</h3>
+                <p class="text-white">Уровень зарплаты выше среднего показателя по России на 23%</p>
+              </div>
             </div>
-            <div class="col-2">
-              <div class="block-100"></div>
-              <h2 class="text-white">6100 тыс. кв. км</h2>
-              <hr class="bg-white">
-              <h3 class="text-white">ПЛОЩАДЬ ДАЛЬНЕГО ВОСТОКА</h3>
-              <p class="text-white">Высокий ВРП - 268 000 руб. (на 19% выше среднего показателя по России)</p>
-
-              <h2 class="text-white mt-4">6,6  чел/кв. км</h2>
-              <hr class="bg-white">
-              <h3 class="text-white">НАСЕЛЕНИЕ РЕГИОНА</h3>
-              <p class="text-white">Первое место в России по объему инвестиций на душу населения</p>
-
-              <h2 class="text-white mt-4">>1 млн. чел</h2>
-              <hr class="bg-white">
-              <h3 class="text-white">ПЛОТНОСТЬ НАСЕЛЕНИЯ</h3>
-              <p class="text-white">Уровень зарплаты выше среднего показателя по России на 23%</p>
-            </div>
-          </div>
           </div>
       </div>
     </div>

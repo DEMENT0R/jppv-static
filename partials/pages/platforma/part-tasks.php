baddf14afe4845d0fed016d34fc7642cba511462
DEMENT0R <dmntr@rambler.ru>
2019-02-11 11:38:05 +1000

    Update part-tasks.php

--- partials/pages/platforma/part-tasks.php
+++ partials/pages/platforma/part-tasks.php
@@ -1,28 +1,30 @@
             <h1>Цели и задачи</h1>
             <h3>Стратегическая цель платформы</h3>
             <p>Повышение благосостояния жителей Дальнего Востока России и улучшение экономического состояния региона</p>
-            <img src="img/platforma/1.jpg" alt="" width="100%">
-            <div class="col-6"><h3>Медицина, увеличение продолжительности жизни</h3></div>
-            <div class="col-6"><p>Сотрудничество в сферах охраны здоровья, профилактики заболеваний, реабилитационной и паллиативной медицины, педиатрии</p></div>
-            <hr>
-            <img src="img/platforma/2.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
-            <img src="img/platforma/3.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
-            <img src="img/platforma/4.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
-            <img src="img/platforma/5.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
-            <img src="img/platforma/6.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
-            <img src="img/platforma/7.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
-            <img src="img/platforma/8.jpg" alt="" width="100%">
-            <p>text</p>
-            <hr>
+            <div class="row">
+                  <img src="img/platforma/1.jpg" alt="" width="100%">
+                  <div class="col-6"><h3>Медицина, увеличение продолжительности жизни</h3></div>
+                  <div class="col-6"><p>Сотрудничество в сферах охраны здоровья, профилактики заболеваний, реабилитационной и паллиативной медицины, педиатрии</p></div>
+                  <hr>
+                  <img src="img/platforma/2.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+                  <img src="img/platforma/3.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+                  <img src="img/platforma/4.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+                  <img src="img/platforma/5.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+                  <img src="img/platforma/6.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+                  <img src="img/platforma/7.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+                  <img src="img/platforma/8.jpg" alt="" width="100%">
+                  <p>text</p>
+                  <hr>
+            </div>

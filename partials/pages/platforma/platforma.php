29b558d6a8cab5fb1514406654054e23ad5d61cd
DEMENT0R <dmntr@rambler.ru>
2019-02-11 10:39:36 +1000

    hide & show blocks

--- partials/pages/platforma/platforma.php
+++ partials/pages/platforma/platforma.php
@@ -8,12 +8,42 @@
             <div class="block-230"></div>
             <div class="block-100"></div>
             <p>
-              <a href="#" class="">Платформа</a><br>
-              <a href="#" class="">Цели и задачи</a><br>
-              <a href="#" class="">Направление деятельности</a><br>
-              <a href="#" class="">Руководство</a><br>
-              <a href="#" class="">JBIK Bank</a>
+              <a href="#" class="" id="link-platforma">Платформа</a><br>
+              <a href="#" class="" id="link-tasks">Цели и задачи</a><br>
+              <a href="#" class="" id="link-actions">Направление деятельности</a><br>
+              <a href="#" class="" id="link-leaders">Руководство</a><br>
+              <a href="#" class="" id="link-bank">JBIK Bank</a>
             </p>
+            <script>
+				function hide_all () {
+					$( "#block-platforma" ).hide();
+					$( "#block-tasks" ).hide();
+					$( "#block-actions" ).hide();
+					$( "#block-leaders" ).hide();
+					$( "#block-bank" ).hide();
+				}
+				$( "#link-platforma" ).click(function() {
+					hide_all ();
+					$( "#block-platforma" ).show();
+				});
+				$( "#link-tasks" ).click(function() {
+					hide_all ();
+					$( "#block-tasks" ).show();
+				});
+				$( "#link-actions" ).click(function() {
+					hide_all ();
+					$( "#block-actions" ).show();
+				});
+				$( "#link-leaders" ).click(function() {
+					hide_all ();
+					$( "#block-leaders" ).show();
+				});
+				$( "#link-bank" ).click(function() {
+					hide_all ();
+					$( "#block-bank" ).show();
+				});
+            	
+            </script>
           </div>
           <div class="col-12 col-md-1 pl-0">
             <div class="block-100"></div>
@@ -21,6 +51,11 @@
           </div>
           <div class="col-12 col-md-6 pl-0">
             <div class="block-100"></div>
+            <div id="block-platforma">1</div>
+            <div id="block-tasks">2</div>
+            <div id="block-actions">3</div>
+            <div id="block-leaders">4</div>
+            <div id="block-bank">5</div>
             <h1>Платформа</h1>
             <p>Способствует интенсификации контактов, обмену информацией, повышению качества этой информации. Станет связующим звеном между японским капиталом и российскими инвестиционными возможностями, проводником и катализатором экономического взаимодействия России и Японии. </p>
             <img src="img/platforma-img-1.svg" alt="" width="100%">

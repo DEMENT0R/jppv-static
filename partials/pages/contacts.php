914805c61c3b85029991ea0a7c4734661a606261
DEMENTOR <dmntr@rambler.ru>
2019-02-13 01:45:14 +1000

    копирайты

--- partials/pages/contacts.php
+++ partials/pages/contacts.php
@@ -20,14 +20,24 @@
                                     <hr>
                                     <div class="row p-2">
                                         <div class="col-3">Социальные сети</div>
-                                        <div class="col-9 text-bold"><a href="#">Facebook</a>&nbsp;
-                                            <p><a href="#">Instagram</a></p>
-                                            <p><a href="#">Vkontakte</a></p>
+                                        <div class="col-9 text-bold">
+                                            <p>
+                                                <a href="#">Facebook</a><br>
+                                                <a href="#">Instagram</a><br>
+                                                <a href="#">Vkontakte</a>
+                                            </p>
                                         </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-9 offset-3">
+                                            <div class="block-100"></div>
+                                            <p class="gray-text">© Все права защищены. ООО"Платформа для поддержки японских инвестиций на Дальнем Востоке"</p>
+                                        </div>
+                                        
                                     </div>
                                 </div>
                                 <div class="col-12 col-md-6" style="padding: 0;">
-                                    <div style="overflow:hidden; width: 100%; height: 100%; position: relative;"><iframe width="100%" height="100%" src="https://maps.google.com/maps?width=100&amp;height=100&amp;hl=ru&amp;q=690091%20%D0%B3.%20%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%B2%D0%BE%D1%81%D1%82%D0%BE%D0%BA%2C%20%20%D0%9E%D0%BA%D0%B5%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C17-%20403%2F1+(JPPV)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><div style="position: absolute;width: 80%;bottom: 10px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;text-align: center;"></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><br />
+                                    <div style="overflow:hidden; width: 100%; height: 100%; position: relative;"><iframe width="100%" height="100%" src="https://maps.google.com/maps?width=100&amp;height=100&amp;hl=ru&amp;q=690091%20%D0%B3.%20%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%B2%D0%BE%D1%81%D1%82%D0%BE%D0%BA%2C%20%20%D0%9E%D0%BA%D0%B5%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C17-%20403%2F1+(JPPV)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><br />
                                 </div>
                             </div>
                         </div>

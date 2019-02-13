78bd5987f98a7d2519c6d792c3cae747f268fc2b
DEMENT0R <dmntr@rambler.ru>
2019-02-13 17:30:43 +1000

    Update contacts.php

--- partials/pages/contacts.php
+++ partials/pages/contacts.php
@@ -3,7 +3,7 @@
                             <div class="row col-12 m-0 p-0">
                                 <div class="block-100 col-12"></div>
                                 <div class="col-md-1"></div>
-                                <div class="col-12 col-md-5">
+                                <div class="col-12 col-md-4">
                                     <h1 class="col-4 offset-3 pl-1">Контакты</h1>
                                     <div class="row p-2">
                                         <div class="col-3">Адрес</div>
@@ -36,7 +36,7 @@
                                         
                                     </div>
                                 </div>
-                                <div class="col-12 col-md-6" style="padding: 0;">
+                                <div class="col-12 col-md-6 offset-md-1" style="padding: 0;">
                                     <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A0b58034bbae1dc457fcd65046a41d9a799a4dc27ea3118fb46c910d41da43848&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
                                     <!-- <div style="overflow:hidden; width: 100%; height: 100%; position: relative;"><iframe width="100%" height="100%" src="https://maps.google.com/maps?width=100&amp;height=100&amp;hl=ru&amp;q=690091%20%D0%B3.%20%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%B2%D0%BE%D1%81%D1%82%D0%BE%D0%BA%2C%20%20%D0%9E%D0%BA%D0%B5%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C17-%20403%2F1+(JPPV)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div> -->
                                 </div>

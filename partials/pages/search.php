78c4d593a323bfd7e7481413d8fd37c41aa36464
DEMENT0R <dmntr@rambler.ru>
2019-02-18 10:00:48 +1000

    search screen

--- partials/pages/search.php
+++ partials/pages/search.php
@@ -4,21 +4,22 @@
           <div class="col-md-1" style="height: 90vh;"></div>
           <div class="col-md-2">
             <div class="block-100"></div>
-            <p class="mt-3 text-breadcrumbs"><a href="/">Главная</a> / <a href="/press-centr">Пресс-центр</a></p>
           </div>
           <div class="col-12 col-md-1 pl-0">
             <div class="block-100"></div>
             <p class="mt-3"><a class="button-back" onclick="javascript:history.back(); return false;" href="#"><img src="img/icons/arrow-back.svg" alt=""> Назад</a></p>
           </div>
-          <?php 
-            
-            if ($_GET['date'] == "") {
-              include 'partials/pages/press-centr/_list.php'; 
-            } else {
-              echo "<div class='col-12 col-md-6 pl-0'><div class='block-100'></div>";
-              include 'partials/pages/press-centr/' . $_GET['date'] . '.php'; 
-              echo "<div class='block-100'></div></div>";
-            }
-          ?>
+
+          <div class="col-md-6 offset-md-4">
+            <h1>Результаты поиска</h1>
+            <div class="block-100"></div>
+            <h2>Раздел с упоминаниями ключевого слова</h2>
+            <hr>
+            <h2>С татья с упоминаниями ключевого слова</h2>
+            <hr>
+            <div class="block-100"></div>
+          </div>
+
+
 
       </div>

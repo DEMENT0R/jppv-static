75895bc65bce1173ba9a3e717937b0dc20a931b1
DEMENT0R <dmntr@rambler.ru>
2019-02-18 10:34:07 +1000

    Update search.php

--- partials/pages/search.php
+++ partials/pages/search.php
@@ -21,30 +21,33 @@
             <h2>Статья с упоминаниями ключевого слова</h2>
             <hr>
             <div class="block-100"></div>
+
+            <?php
+              searchFiles('partials/pages', $_GET['query']);
+
+              function searchFiles($dir, $search) {
+                $files = glob($dir."/*.php"); // Получаем все php-файлы из директории
+                $results = array(); // Создаём массив для результатов поиска
+                for ($i = 0; $i < count($files); $i++) {
+                  echo "Ищем ".$search." в ";
+                  print_r($files[$i]);
+                  echo "<br>";
+                  /* Перебираем все полученные файлы */
+                  $str = strip_tags(file_get_contents($files[$i])); // Помещаем содержимое файлов в переменную, предварительно убрав все html-теги
+                  $count = substr_count($str, $search); // Ищем количество вхождений искомой строки в файл
+                  if ($count) $results[$files[$i]] = $count; // Если хотя бы 1 вхождение найдено, то добавляем файл с количеством вхождений в массив результатов
+                }
+                return $results; // Возвращаем результат
+              }
+              $results = searchFiles(".", "Платформа"); // Ищем файлы в текущей директории, в которых входит строка "ABC"
+              var_dump($results); // Выводим массив
+            ?>
+
+            <script>
+              console.log (<?php print_r($results); ?>);
+            </script>
+
+            <hr>
+            <div class="block-100"></div>
           </div>
-
-      <?php
-        searchFiles('partials/pages', $_GET['query']);
-
-        function searchFiles($dir, $search) {
-          $files = glob($dir."/*.php"); // Получаем все php-файлы из директории
-          $results = array(); // Создаём массив для результатов поиска
-          for ($i = 0; $i < count($files); $i++) {
-            print_r($files[$i]);
-            echo "<br>";
-            /* Перебираем все полученные файлы */
-            $str = strip_tags(file_get_contents($files[$i])); // Помещаем содержимое файлов в переменную, предварительно убрав все html-теги
-            $count = substr_count($str, $search); // Ищем количество вхождений искомой строки в файл
-            if ($count) $results[$files[$i]] = $count; // Если хотя бы 1 вхождение найдено, то добавляем файл с количеством вхождений в массив результатов
-          }
-          return $results; // Возвращаем результат
-        }
-        $results = searchFiles(".", "Платформа"); // Ищем файлы в текущей директории, в которых входит строка "ABC"
-        var_dump($results); // Выводим массив
-      ?>
-
-      <script>
-        console.log (<?php print_r($results); ?>);
-      </script>
-
       </div>
